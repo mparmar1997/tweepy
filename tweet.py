2507e5479c1096256c900f6a6439381bc8963e73
Manvendra singh <45799881+mparmar1997@users.noreply.github.com>
2019-01-18 21:39:40 +0530

    Add files via upload

---
+++ tweet.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import tweepy
+import os
+
+# consumer keys and access tokens, used for OAuth
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# creation of the actual interface, using authentication
+api = tweepy.API(auth)
+#api.update_status('Test')
+
+
+#!!!Get information about yourself!!!
+
+user = api.me()
+
+print('Name: ' + user.name)
+print('Location: ' + user.location)
+print('Friends: ' + str(user.friends_count))
+
+
+#!!!Get information about any user!!!
+
+user = api.get_user("@mparmar1997")
+
+print('Name: ' + user.name)
+print('Location: ' + user.location)
+print('Friends: ' + str(user.friends_count))
+
+
+#!!!Search tweet!!!
+
+tweet=input("Enter whatever you wanna search here:  ")
+search = tweepy.Cursor(api.search, q=tweet, lang="ne").items(30000)
+
+#!!!Printing tweets!!!
+
+for item in search:
+    print (item)
+
+
